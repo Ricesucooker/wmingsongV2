656e19a3426aa3b683401b0691f8082e4cd09463
Matcha  MacBook.air <pockynekokun@gmail.com>
2025-08-20 04:11:39 +0100

    test CI/CD fix .jpg to .JPG

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,7 +23,7 @@ import nobrainIcon from "../assets/images/nobrain_icon.jpg";
 
 //images
 import topPicture from "../assets/images/topSprings.jpg";
-import wMing from "../assets/images/wming.jpg";
+import wMing from "../assets/images/wming.JPG";
 import photoOne from  "../assets/images/photo1.jpg";
 import photoTwo from  "../assets/images/photo2.jpg";
 import photoThree from  "../assets/images/photo3.jpg";
